8e0192d8e46eb9f59823e825536d2e0a3ff98604
oscarjakobsson <oscar.jakobsson@lexicon.se>
2016-01-28 09:54:45 +0100

    init

---
+++ TestMiniMethods-RecursiveEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMiniMethods_RecursiveEven {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(RecursiveEven(10));
+        }
+
+        static int RecursiveEven(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            return RecursiveEven(n - 1) + 2;
+        }
+
+    }
+}
